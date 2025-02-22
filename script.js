194e504972fa4761327242838abed06a5545aca2
aaronshi1129 <124439445+aaronshi1129@users.noreply.github.com>
2025-02-23 00:10:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
   gold: 'https://alittlemoreenglish.weebly.com/uploads/2/6/6/3/26638990/gold-medal_orig.png',
   silver: 'https://alittlemoreenglish.weebly.com/uploads/2/6/6/3/26638990/silver-medal_orig.png',
   bronze: 'https://alittlemoreenglish.weebly.com/uploads/2/6/6/3/26638990/bronze-medal-orig_orig.jpg'
-};
+  };
 
   let score = 0;
   let countriesData;
